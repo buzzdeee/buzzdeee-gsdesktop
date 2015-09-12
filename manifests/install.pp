02aff717cc927dc323c7816d7d8f28474ba48641
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2015-09-12 14:02:13 +0200

    Add missing ,

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,7 +19,7 @@ class gsdesktop::install (
         ensure        => present,
         url           => "${gsapp_url}/${values['file']}",
         target        => '/usr/local/libexec/GNUstep',
-        root_dir      => "${app}.app"
+        root_dir      => "${app}.app",
         digest_string => $values['sha512sum'],
         digest_type   => 'sha512',
         require       => Package[$package_name],
